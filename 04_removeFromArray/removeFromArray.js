396e205495f04da1f66151e251467720b30cbf09
Zardoz8901 <interzoneclearinghouse@gmail.com>
2022-04-11 15:37:18 +0200

    Cheat 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(...theArgs) {
+    const array = theArgs[0];
+    return array.filter(val => !theArgs.includes(val))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
